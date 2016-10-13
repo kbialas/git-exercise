020779f9bfcb62d5b3d4b4d44d61d27720bc4606
Not Appropriate Username <myprivateemail@example.com>
2016-10-13 10:36:54 +0200

    Create and use list component

--- app/src/routes/home/home.component.js
+++ app/src/routes/home/home.component.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {Newsletter} from './newsletter/newsletter.component';
+import {List} from './list/list.component';
 
 export class Home extends Component {
   render() {
@@ -26,6 +28,14 @@ export class Home extends Component {
             nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
             anim id est laborum.
           </div>
+        </section>
+
+        <section className="home__section">
+          <Newsletter/>
+        </section>
+
+        <section className="home__section">
+          <List/>
         </section>
       </div>
     );
